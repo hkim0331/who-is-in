d928496cdc4ed89418d44cdd8373aecb22ebc4f3
Hiroshi Kimura <hiroshi.kimura.0331@gmail.com>
2017-12-06 12:08:35 +0900

    define renumber.rb

---
+++ renumber.rb
@@ -0,0 +1,24 @@
+#!/usr/bin/env ruby
+# coding: utf-8
+
+def usage
+  puts <<EOF
+usage: #{$0} dir
+dir 中の *.jpg ファイルを 001 からの連番にリネームする。
+EOF
+  exit
+end
+
+usage unless ARGV.length == 1
+
+Dir.chdir(ARGV[0]) do
+  all = Dir.glob("*.jpg").sort
+  ("0001".."9999").each do |name|
+    f = all.shift
+    if f.nil?
+      break
+    else
+      File.rename(f,"#{name}.jpg")
+    end
+  end
+end
--- reverse.rb
+++ reverse.rb
@@ -1,20 +1,33 @@
 #!/usr/bin/env ruby
+# coding: utf-8
+
+def usage
+  puts <<EOF
+usage:
+{$0} from-dir dest-dir
+
+from-dir 中の *.jpg ファイルを逆順の名前に変えて dest-dir に移動する。
+EOF
+  exit
+end
 
 require 'logger'
 log = Logger.new("log/who-is-in.log", 5, 10*1024)
 log.level = Logger::DEBUG
 
-orig = Dir.glob("images/*.jpg").sort
-rev = orig.reverse.map{ |x| x.sub(/images/,"reverse")}
+if (ARGV.length==2)
+  from = ARGV[0]
+  dest = ARGV[1]
+else
+  usage
+end
 
-# NO?
-orig.each do |from|
+orig = Dir.glob("#{from}/*.jpg").sort
+rev = orig.reverse.map{ |x| x.sub(/#{from}/, dest) }
+
+orig.each do |o|
   to = rev.shift
-  File.rename(from, to)
+  File.rename(o, to)
   log.debug("copied from #{from} to #{to}")
 end
 
-# while (from = orig.shift)
-#   to = rev.shift
-#   File.rename(from, to)
-# end
